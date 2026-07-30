ef834af508691032c1e571bf79a4bf0a5f0cf6c6
ell1010 <elliottbarrett25@gmail.com>
2026-07-30 16:01:50 +0100

    more P65 fixes, will revert if it breaks prev commit

--- keyboards/gmmk/gmmk3/p65/ansi/keymaps/viahybrid/keymap.c
+++ keyboards/gmmk/gmmk3/p65/ansi/keymaps/viahybrid/keymap.c
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include QMK_KEYBOARD_H
 
 /*
@@ -23,7 +22,7 @@
 * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┘
 * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │
 * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┐
-* │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │ Enter  │PgD│
+* │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │ Enter  │PgU│
 * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
 * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │ Shift│ ↑ │End│
 * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬─┬───┼───┼───┤
@@ -71,16 +70,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_PGUP,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_END,
-        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                       KC_RALT,  MO(_FL),         KC_LEFT,  KC_DOWN,  KC_RGHT
+        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                       KC_RALT,  MO(_FL),            KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
 
     /* Function Layer */
     [_FL] = LAYOUT(
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,   _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  ORGB,  KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_INS,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  ORGB,     KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_INS,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  KC_PGDN,
         _______,            RM_SATD,  RM_SATU,  RM_SPDD,  RM_SPDU,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  RM_VALU,  KC_HOME,
-        _______,  GU_TOGG,  _______,                                _______,                      _______,  _______,            RM_PREV, RM_VALD,  RM_NEXT
+        _______,  GU_TOGG,  _______,                                _______,                      _______,  _______,            RM_PREV,  RM_VALD,  RM_NEXT
     ),
 
     /* Custom Layer */
